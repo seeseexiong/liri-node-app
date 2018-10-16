7679d82dff59694dd75bcc710c23d39b888e3e97
See Xiong <seeseexiong@gmail.com>
2018-10-15 23:34:01 -0500

    3rd push

--- liri/liri.js
+++ liri/liri.js
@@ -83,24 +83,25 @@ var commandInput  = process.argv[2];
   if (commandInput === "movie-this") {
     // Include the request npm package
     let request = require('request');
-
+      
     //Take the arguement and use that as the search keyword
     let searchTerm = process.argv[2];
-
+      
     //Run a request to the OMDB API with the movie specified
     let queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
-
+      
     //Debug against the actual URL.
     console.log(queryUrl);
-
+      
+      
     // Create a request to the queryUrl
     request(queryUrl, function(error, response, body) {
     if (error) console.log('error:', error); //print the error if one occurred
-
+  
     console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
     let JSONResponse = JSON.parse(body);
     //console.log(JSONResponse)
-
+  
     // Print the following information
     // Movie Title
     console.log('Title:', JSONResponse.Title);
@@ -117,7 +118,6 @@ var commandInput  = process.argv[2];
     //Plot
     console.log('Plot:', JSONResponse.Plot);
     //Actors
-    console.log('Actors:', JSONResponse.Actors);  
+    console.log('Actors:', JSONResponse.Actors);
     });
-  }
-  
+}
